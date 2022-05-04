38be002004e0886546a898583d4f4b48fc16622c
werdien jihed <informatique.jihed@gmail.com>
2022-05-04 22:31:07 +0100

    add simple validation for req params

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,31 +1,43 @@
 import QuizModel from "../models/quizModel.js";
+import mongoose from "mongoose";
 
-export const getQuizzes = async (req, res) => {
+export const getQuizzes = async (req, res, next) => {
   const page = req.query.page || 0;
   const limit = req.query.limit || 0;
 
   const quizzes = await QuizModel.find()
     .skip(page * limit)
-    .limit(limit);
+    .limit(limit)
+    .catch((err) => {
+      return next(err);
+    });
+
   res.json(quizzes);
 };
 
 export const getQuiz = async (req, res, next) => {
-  try {
-    const quiz = await QuizModel.findById(req.params.id);
-    res.json(quiz);
-  } catch (error) {
-    next(error);
+  const valid = mongoose.isValidObjectId(req.params.id);
+
+  if (!valid) {
+    const error = new Error(`Invalid id: ${req.params.id}`);
+    return next(error);
   }
+
+  const quiz = await QuizModel.findById(req.params.id).catch((err) => {
+    next(err);
+  });
+
+  res.json(quiz);
 };
 
 export const getRandomQuiz = async (req, res, next) => {
-  try {
-    const quiz = await QuizModel.aggregate().sample(1);
-    res.json(quiz[0]);
-  } catch (error) {
-    next(error);
-  }
+  const quiz = await QuizModel.aggregate()
+    .sample(1)
+    .catch((err) => {
+      return next(err);
+    });
+
+  res.json(quiz[0]);
 };
 
 export const getRandomQuizzes = async (req, res, next) => {
@@ -38,43 +50,69 @@ export const getRandomQuizzes = async (req, res, next) => {
     filter.topic = req.query.topic;
   }
 
-  try {
-    const quiz = await QuizModel.aggregate()
-      .match(filter)
-      .sample(Number(req.params.count));
-    res.json(quiz);
-  } catch (error) {
-    next(error);
-  }
+  const quiz = await QuizModel.aggregate()
+    .match(filter)
+    .sample(Number(req.params.count))
+    .catch((err) => {
+      return next(err);
+    });
+
+  res.json(quiz);
 };
 
 export const createQuiz = async (req, res, next) => {
-  try {
-    const quiz = new QuizModel(req.body);
-    await quiz.save();
-    res.json(quiz);
-  } catch (error) {
-    next(error);
+  const valid = await QuizSchema.isValid(req.body);
+
+  if (!valid) {
+    const error = new Error(`Invalid quiz!`);
+    res.status(400);
+    return next(error);
   }
+
+  const quiz = new QuizModel(req.body);
+  await quiz.save().catch((err) => {
+    next(err);
+  });
+
+  res.json(quiz);
 };
 
 export const updateQuiz = async (req, res, next) => {
-  try {
-    const quiz = await QuizModel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.json(quiz);
-  } catch (error) {
-    next(error);
+  const validId = mongoose.isValidObjectId(req.params.id);
+  const validQuiz = await QuizSchema.isValid(req.body);
+
+  if (!validId) {
+    const error = new Error(`Invalid id: ${req.params.id}`);
+    res.status(400);
+    return next(error);
   }
+
+  if (!validQuiz) {
+    const error = new Error(`Invalid quiz!`);
+    res.status(400);
+    return next(error);
+  }
+
+  const quiz = await QuizModel.findByIdAndUpdate(req.params.id, req.body).catch(
+    (err) => {
+      next(err);
+    }
+  );
+  res.json(quiz);
 };
 
 export const deleteQuiz = async (req, res, next) => {
-  try {
-    await QuizModel.findByIdAndDelete(req.params.id);
-    res.json({ id: req.params.id });
-  } catch (error) {
-    next(error);
+  const valid = mongoose.isValidObjectId(req.params.id);
+
+  if (!valid) {
+    const error = new Error(`Invalid id: ${req.params.id}`);
+    res.status(400);
+    return next(error);
   }
+
+  await QuizModel.findByIdAndDelete(req.params.id).catch((err) => {
+    next(err);
+  });
+
+  res.json({ id: req.params.id });
 };
